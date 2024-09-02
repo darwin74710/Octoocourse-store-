4757b44c9e27ddcaccbad41b748082c28be23dad
AlliSerna23231 <allison_serna23231@elpoli.edu.co>
2024-09-02 00:19:50 -0500

    Actualización del login y la creación de la app register

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('ventas/', views.ventas, name='ventas'),
 ]
--- core/views.py
+++ core/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'core/home.html')
+
+
+def ventas(request):
+    return render(request, 'core/ventas.html')
