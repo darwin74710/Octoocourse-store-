b518b2724a5e61147799c1537acf4c8c7d2ca8a5
Darwin <darwin74710@gmail.com>
2024-09-12 15:43:35 -0500

    Se hacen mejoras en el responsive

--- estudiantes/static/estudiantes/js/homeScript.js
+++ estudiantes/static/estudiantes/js/homeScript.js
@@ -13,6 +13,16 @@ function ShowUser(){
     }
 }
 
+function ShowOptions(){
+    var menu_despleg = document.getElementsByClassName("menu-movil")[0];
+    
+    if(menu_despleg.style.display == "none" || menu_despleg.style.display != "block"){
+        menu_despleg.style.display = "block";
+    }else{
+        menu_despleg.style.display = "none";
+    }
+}
+
 function ShowNoti(){
     var usu_despleg = document.getElementsByClassName("desp-usuario")[0];
     var noti_despleg = document.getElementsByClassName("desp-notifi")[0];
