ff4821986c515ea85d7aa7d53e52f40919cc1fa1
Darwin <darwin74710@gmail.com>
2024-09-17 20:04:54 -0500

    Se crea la interfaz de los cursos

--- estudiantes/views.py
+++ estudiantes/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'estudiantes/home.html')
+
+def config(request):
+    return render(request, 'estudiantes/config.html')
