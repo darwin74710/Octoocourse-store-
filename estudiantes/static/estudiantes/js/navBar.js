66fc5294d2a6371e4962ad84a165a5b9aecdf397
Darwin <darwin74710@gmail.com>
2024-09-21 23:50:53 -0500

    Se complementa la ventana del estudiante

--- estudiantes/static/estudiantes/js/navBar.js
+++ estudiantes/static/estudiantes/js/navBar.js
@@ -1,11 +1,11 @@
 function MenuPC(){
     var menu_pc = document.getElementsByClassName("menu-compu")[0];
-    var menu_noti = document.getElementsByClassName("menu-notifi")[0];
+    var menu_movil = document.getElementsByClassName("menu-movil")[0];
 
-    if(menu_noti.style.display != "none"){
-        menu_noti.style.display = "none";
+    if(menu_movil.style.display != "none"){
+        menu_movil.style.display = "none";
     }
-    
+
     if(menu_pc.style.display == "none" || menu_pc.style.display != "block"){
         menu_pc.style.display = "block";
     }else{
@@ -14,7 +14,12 @@ function MenuPC(){
 }
 
 function MenuMovil(){
+    var menu_pc = document.getElementsByClassName("menu-compu")[0];
     var menu_movil = document.getElementsByClassName("menu-movil")[0];
+
+    if(menu_pc.style.display != "none"){
+        menu_pc.style.display = "none";
+    }
 
     if(menu_movil.style.display == "none" || menu_movil.style.display != "block"){
         menu_movil.style.display = "block";
@@ -23,76 +28,36 @@ function MenuMovil(){
     }
 }
 
-function MenuNoti(){
-    var menu_pc = document.getElementsByClassName("menu-compu")[0];
-    var menu_noti = document.getElementsByClassName("menu-notifi")[0];
-
-    if(menu_pc.style.display != "none"){
-        menu_pc.style.display = "none";
-    }
-
-    if(menu_noti.style.display == "none" || menu_noti.style.display != "block"){
-        menu_noti.style.display = "block";
-    }else{
-        menu_noti.style.display = "none";
-    }
-}
-
-function MenuEstandar(){
+function Navegacion(nav){
     var estandar = document.getElementsByClassName("estandar")[0];
     var cursos = document.getElementsByClassName("cursos")[0];
     var empleo = document.getElementsByClassName("empleo")[0];
+    var hojaVida = document.getElementsByClassName("hojaVida")[0];
+    var config = document.getElementsByClassName("config")[0];
+    var des_ofertas = document.getElementsByClassName("des_ofertas")[0];
+    var des_cursos = document.getElementsByClassName("des_cursos")[0];
 
-    if(empleo.style.display != "none"){
-        empleo.style.display = "none";
-    }
-    if(cursos.style.display != "none"){
-        cursos.style.display = "none";
-    }
+    estandar.style.display = "none";
+    cursos.style.display = "none";
+    empleo.style.display = "none";
+    hojaVida.style.display = "none";
+    config.style.display = "none";
+    des_ofertas.style.display = "none";
+    des_cursos.style.display = "none";
 
-    if(estandar.style.display == "none" || estandar.style.display != "block"){
+    if(nav === 'estandar'){
         estandar.style.display = "block";
-    }
-}
-
-function MenuCursos(){
-    var estandar = document.getElementsByClassName("estandar")[0];
-    var cursos = document.getElementsByClassName("cursos")[0];
-    var empleo = document.getElementsByClassName("empleo")[0];
-
-    if(empleo.style.display != "none"){
-        empleo.style.display = "none";
-    }
-    if(estandar.style.display != "none"){
-        estandar.style.display = "none";
-    }
-
-    if(cursos.style.display == "none" || cursos.style.display != "block"){
+    }else if(nav === 'cursos'){
         cursos.style.display = "block";
-    }
-}
-
-function MenuEmpleo(){
-    var estandar = document.getElementsByClassName("estandar")[0];
-    var cursos = document.getElementsByClassName("cursos")[0];
-    var empleo = document.getElementsByClassName("empleo")[0];
-
-    if(estandar.style.display != "none"){
-        estandar.style.display = "none";
-    }
-    if(cursos.style.display != "none"){
-        cursos.style.display = "none";
-    }
-
-    if(empleo.style.display == "none" || empleo.style.display != "block"){
+    }else if(nav === 'empleo'){
         empleo.style.display = "block";
+    }else if(nav === 'hojaVida'){
+        hojaVida.style.display = "block";
+    }else if(nav === 'config'){
+        config.style.display = "block";
+    }else if(nav === 'des_ofertas'){
+        des_ofertas.style.display = "block";
+    }else if(nav === 'des_cursos'){
+        des_cursos.style.display = "block";
     }
-}
-
-function Exit(){
-    var menu_pc = document.getElementsByClassName("menu-compu")[0];
-    var menu_movil = document.getElementsByClassName("menu-movil")[0];
-
-    menu_pc.style.display = "none";
-    menu_movil.style.display = "none";
 }
