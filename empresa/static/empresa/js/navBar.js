f7496cf78689311d6ecade65c5de0a53374d8175
AlliSerna23231 <allison_serna23231@elpoli.edu.co>
2024-09-22 03:02:47 -0500

    actualizacion-detalles interfaz empresa

--- empresa/static/empresa/js/navBar.js
+++ empresa/static/empresa/js/navBar.js
@@ -1,95 +1,153 @@
-function MenuPC(){
+function MenuPC() {
     var menu_pc = document.getElementsByClassName("menu-compu")[0];
-    var menu_noti = document.getElementsByClassName("menu-notifi")[0];
 
-    if(menu_noti.style.display != "none"){
-        menu_noti.style.display = "none";
-    }
-    
-    if(menu_pc.style.display == "none" || menu_pc.style.display != "block"){
+    if (menu_pc.style.display == "none" || menu_pc.style.display != "block") {
         menu_pc.style.display = "block";
-    }else{
+    } else {
         menu_pc.style.display = "none";
     }
 }
 
-function MenuMovil(){
+function MenuMovil() {
     var menu_movil = document.getElementsByClassName("menu-movil")[0];
 
-    if(menu_movil.style.display == "none" || menu_movil.style.display != "block"){
+    if (menu_movil.style.display == "none" || menu_movil.style.display != "block") {
         menu_movil.style.display = "block";
-    }else{
+    } else {
         menu_movil.style.display = "none";
     }
 }
 
-function MenuNoti(){
-    var menu_pc = document.getElementsByClassName("menu-compu")[0];
-    var menu_noti = document.getElementsByClassName("menu-notifi")[0];
-
-    if(menu_pc.style.display != "none"){
-        menu_pc.style.display = "none";
-    }
-
-    if(menu_noti.style.display == "none" || menu_noti.style.display != "block"){
-        menu_noti.style.display = "block";
-    }else{
-        menu_noti.style.display = "none";
-    }
-}
-
-function MenuEstandar(){
+function MenuEstandar() {
     var estandar = document.getElementsByClassName("estandar")[0];
     var estudiantes = document.getElementsByClassName("listaEstud")[0];
     var empleo = document.getElementsByClassName("empleo")[0];
+    var config = document.getElementsByClassName("config")[0];
+    var perfil = document.getElementsByClassName("perfil")[0];
 
-    if(empleo.style.display != "none"){
+
+    if (empleo.style.display != "none") {
         empleo.style.display = "none";
     }
-    if(estudiantes.style.display != "none"){
+    if (estudiantes.style.display != "none") {
         estudiantes.style.display = "none";
     }
+    if (config.style.display != "none") {
+        config.style.display = "none";
+    }
+    if (perfil.style.display != "none") {
+        perfil.style.display = "none";
+    }
 
-    if(estandar.style.display == "none" || estandar.style.display != "block"){
+    if (estandar.style.display == "none" || estandar.style.display != "block") {
         estandar.style.display = "block";
     }
 }
 
-function MenuCursos(){
+function MenuCursos() {
     var estandar = document.getElementsByClassName("estandar")[0];
     var estudiantes = document.getElementsByClassName("listaEstud")[0];
     var empleo = document.getElementsByClassName("empleo")[0];
+    var config = document.getElementsByClassName("config")[0];
+    var perfil = document.getElementsByClassName("perfil")[0];
 
-    if(empleo.style.display != "none"){
+
+    if (empleo.style.display != "none") {
         empleo.style.display = "none";
     }
-    if(estandar.style.display != "none"){
+    if (estandar.style.display != "none") {
         estandar.style.display = "none";
     }
+    if (config.style.display != "none") {
+        config.style.display = "none";
+    }
+    if (perfil.style.display != "none") {
+        perfil.style.display = "none";
+    }
 
-    if(estudiantes.style.display == "none" || estudiantes.style.display != "block"){
+    if (estudiantes.style.display == "none" || estudiantes.style.display != "block") {
         estudiantes.style.display = "block";
     }
 }
 
-function MenuEmpleo(){
+function MenuEmpleo() {
     var estandar = document.getElementsByClassName("estandar")[0];
     var estudiantes = document.getElementsByClassName("listaEstud")[0];
     var empleo = document.getElementsByClassName("empleo")[0];
+    var config = document.getElementsByClassName("config")[0];
+    var perfil = document.getElementsByClassName("perfil")[0];
 
-    if(estandar.style.display != "none"){
+
+    if (estandar.style.display != "none") {
         estandar.style.display = "none";
     }
-    if(estudiantes.style.display != "none"){
+    if (estudiantes.style.display != "none") {
         estudiantes.style.display = "none";
     }
+    if (config.style.display != "none") {
+        config.style.display = "none";
+    }
+    if (perfil.style.display != "none") {
+        perfil.style.display = "none";
+    }
 
-    if(empleo.style.display == "none" || empleo.style.display != "block"){
+    if (empleo.style.display == "none" || empleo.style.display != "block") {
         empleo.style.display = "block";
     }
 }
 
-function Exit(){
+function MenuConfig() {
+    var estandar = document.getElementsByClassName("estandar")[0];
+    var estudiantes = document.getElementsByClassName("listaEstud")[0];
+    var empleo = document.getElementsByClassName("empleo")[0];
+    var config = document.getElementsByClassName("config")[0];
+    var perfil = document.getElementsByClassName("perfil")[0];
+
+
+    if (estandar.style.display != "none") {
+        estandar.style.display = "none";
+    }
+    if (estudiantes.style.display != "none") {
+        estudiantes.style.display = "none";
+    }
+    if (empleo.style.display != "none") {
+        empleo.style.display = "none";
+    }
+    if (perfil.style.display != "none") {
+        perfil.style.display = "none";
+    }
+    
+    if (config.style.display == "none" || config.style.display != "block") {
+        config.style.display = "block";
+    }
+}
+
+function MenuPerfil() {
+    var estandar = document.getElementsByClassName("estandar")[0];
+    var estudiantes = document.getElementsByClassName("listaEstud")[0];
+    var empleo = document.getElementsByClassName("empleo")[0];
+    var config = document.getElementsByClassName("config")[0];
+    var perfil = document.getElementsByClassName("perfil")[0];
+
+
+    if (estandar.style.display != "none") {
+        estandar.style.display = "none";
+    }
+    if (estudiantes.style.display != "none") {
+        estudiantes.style.display = "none";
+    }
+    if (empleo.style.display != "none") {
+        empleo.style.display = "none";
+    }
+    if (config.style.display != "none") {
+        config.style.display = "none";
+    }
+    if (perfil.style.display == "none" || perfil.style.display != "block") {
+        perfil.style.display = "block";
+    }
+}
+
+function Exit() {
     var menu_pc = document.getElementsByClassName("menu-compu")[0];
     var menu_movil = document.getElementsByClassName("menu-movil")[0];
 
