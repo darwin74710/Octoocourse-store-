35e34c2d0b7ccce120cfdc6e1c0f9c64ee70d2a7
AlliSerna23231 <allison_serna23231@elpoli.edu.co>
2024-10-04 08:05:42 -0500

    Se modifica la lista de estudiantes

--- empresa/static/empresa/js/script.js
+++ empresa/static/empresa/js/script.js
@@ -1,20 +1,47 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    slidesPerGroup: 3,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+let swiper;
 
-
+function initializeSwiper() {
+    const screenWidth = window.innerWidth;
   
+    if (screenWidth > 800) {
+        swiper = new Swiper(".mySwiper", {
+            slidesPerView: 3,
+            spaceBetween: 30,
+            slidesPerGroup: 3,
+            loop: true,
+            loopFillGroupWithBlank: true,
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+        });
+    } else {
+        swiper = new Swiper('.mySwiper', {
+            slidesPerView: 1,
+            spaceBetween: 10, 
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        });
+    }
+}
 
-  
+// Inicializa el Swiper al cargar la página
+initializeSwiper();
+
+// Actualiza el Swiper al cambiar el tamaño de la ventana
+window.addEventListener('resize', function () {
+    if (swiper) {
+        swiper.destroy(); 
+    }
+    initializeSwiper(); 
+});
