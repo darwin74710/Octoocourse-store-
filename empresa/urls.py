662dbcccb84b186d9a74cd304552249ab20c6d15
AlliSerna23231 <allison_serna23231@elpoli.edu.co>
2024-10-10 16:30:17 -0500

    update 10 oct

--- empresa/urls.py
+++ empresa/urls.py
@@ -2,6 +2,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.empresa, name='empresa'),
+    path('', views.inicioE, name='inicioE'),
     path('about-me/', views.aboutMe, name='aboutMeStudent'), 
+    path('estudiantes/', views.estudiantesE, name='estudiantesE'), 
+    path('configE/', views.configE, name='configE'), 
+    path('ofertasE/', views.ofertasE, name='ofertasE'), 
+    path('des_ofertas/', views.des_ofertas, name='des_ofertas'), 
+    path('listEAp/', views.listEAp, name='listEAp'), 
+
+
+
+
+
+
 ]
