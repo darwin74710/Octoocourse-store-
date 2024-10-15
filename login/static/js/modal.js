a7c24ce6b6d40f7c120fd68bddf1dae2407e900a
AlliSerna23231 <allison_serna23231@elpoli.edu.co>
2024-10-15 11:08:46 -0500

    cambios en login, register, ofertasE e implementación de config en la app de empresas

--- login/static/js/modal.js
+++ login/static/js/modal.js
@@ -1,24 +1,18 @@
- // Obtener el modal
  var modal = document.getElementById("forgot-password-modal");
 
- // Obtener el enlace que abre el modal
  var link = document.getElementById("forgot-password-link");
 
- // Obtener el elemento <span> que cierra el modal
  var span = document.getElementsByClassName("close")[0];
 
- // Cuando el usuario hace clic en el enlace, abre el modal
  link.onclick = function(event) {
      event.preventDefault();
      modal.style.display = "block";
  }
 
- // Cuando el usuario hace clic en <span> (x), cierra el modal
  span.onclick = function() {
      modal.style.display = "none";
  }
 
- // Cuando el usuario hace clic en cualquier lugar fuera del modal, cierra el modal
  window.onclick = function(event) {
      if (event.target == modal) {
          modal.style.display = "none";
