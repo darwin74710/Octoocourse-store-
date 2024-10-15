a724b8b7f3b01aa20f61b58d7449191a50ac7690
Darwin <darwin74710@gmail.com>
2024-10-14 20:17:33 -0500

    Se habilida el home como ventana principal

--- login/urls.py
+++ login/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.inicioS, name='inicioS'),
-
+    path('', views.home, name='home'),
+    path('login/', views.inicioS, name='inicioS'),
 ]
